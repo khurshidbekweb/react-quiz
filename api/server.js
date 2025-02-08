cd65507ea3be97f9e9f7d42b8db64d3ed58feec1
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2025-02-08 13:52:26 +0500

    adds

---
+++ api/server.js
@@ -0,0 +1,10 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("data/questions.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+module.exports = server;
+npm install -g vercel
